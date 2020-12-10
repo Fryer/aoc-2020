45aaa3b93c50521f61c928ba3f32797e8eff1d40
Fryer <Fryer@users.noreply.github.com>
2020-12-10 18:07:48 +0100

    Day 10

--- aoc.js
+++ aoc.js
@@ -405,6 +405,39 @@ days[9] = function(data) {
 };
 
 
+days[10] = function(data) {
+    // Part 1:
+    {
+        let adapters = data.split('\n').map(jolts => jolts * 1).sort((a, b) => a - b);
+        let distribution = [0, 0, 0, 0];
+        adapters.slice(1).forEach((jolts, i) => distribution[jolts - adapters[i]]++);
+        let multiplied = (distribution[1] + 1) * (distribution[3] + 1);
+        log('Multiplied distribution (part 1): ' + multiplied);
+    }
+    
+    // Part 2:
+    {
+        let chain = [0].concat(data.split('\n').map(jolts => jolts * 1).sort((a, b) => a - b));
+        chain.push(chain[chain.length - 1] + 3);
+        function subArrangements(chain) {
+            return chain.slice(1, -1).reduce((total, jolts, i) => total + (chain[i + 1] - chain[i - 1] <= 3 ? subArrangements([chain[i - 1]].concat(chain.slice(i + 1))) : 0), 1);
+        }
+        function arrangements(chain) {
+            let total = 1;
+            let subChainStart = 0;
+            for (let i = 1; i < chain.length; i++) {
+                if (chain[i] - chain[i - 1] == 3) {
+                    total *= subArrangements(chain.slice(subChainStart, i + 1));
+                    subChainStart = i;
+                }
+            }
+            return total;
+        }
+        log('Possible arrangements (part 2): ' + arrangements(chain));
+    }
+};
+
+
 async function run() {
     document.querySelector('pre').textContent = '';
     let day = location.hash.slice(1);
