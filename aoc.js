970f0cdba4b081b50394e3024faf796056cbdfda
Fryer <Fryer@users.noreply.github.com>
2020-12-11 07:13:47 +0100

    Day 11

--- aoc.js
+++ aoc.js
@@ -438,6 +438,106 @@ days[10] = function(data) {
 };
 
 
+days[11] = function(data) {
+    // Part 1:
+    {
+        let rows = data.split('\n');
+        let w = rows[0].length;
+        let h = rows.length;
+        let newSeats = data.replaceAll('\n', '');
+        let seats = '';
+        while (seats != newSeats) {
+            seats = newSeats;
+            newSeats = '';
+            seats.split('').forEach((seat, i) => {
+                if (seat == '.') {
+                    newSeats += '.';
+                    return;
+                }
+                let offsets = [];
+                if (i % w > 0) offsets.push(-1);
+                if (i % w < w - 1) offsets.push(1);
+                if (i >= w) offsets.push(-w);
+                if (i < h * w - w) offsets.push(w);
+                if (i % w > 0 && i >= w) offsets.push(-w - 1);
+                if (i % w < w - 1 && i >= w) offsets.push(-w + 1);
+                if (i % w > 0) offsets.push(w - 1);
+                if (i % w < w - 1) offsets.push(w + 1);
+                let adjacent = offsets.reduce((adjacent, offset) => adjacent + (i + offset >= 0 && i + offset < seats.length ? seats[i + offset] == '#' : 0), 0);
+                if (seat == 'L' && adjacent == 0) {
+                    newSeats += '#';
+                }
+                else if (seat == '#' && adjacent >= 4) {
+                    newSeats += 'L';
+                }
+                else {
+                    newSeats += seat;
+                }
+            });
+        }
+        let occupied = seats.split('').reduce((occupied, seat) => occupied + (seat == '#'), 0);
+        log('Occupied seats (part 1): ' + occupied);
+    }
+    
+    // Part 2:
+    {
+        let rows = data.split('\n');
+        let w = rows[0].length;
+        let h = rows.length;
+        let newSeats = data.replaceAll('\n', '');
+        let seatOffsets = newSeats.split('').map((seat, i) => {
+            function trace(x, y, dx, dy) {
+                x += dx;
+                y += dy;
+                while (x >= 0 && y >= 0 && x < w && y < h) {
+                    let i = y * w + x;
+                    if (newSeats[i] != '.') {
+                        return [i];
+                    }
+                    x += dx;
+                    y += dy;
+                }
+                return [];
+            }
+            let x = i % w;
+            let y = Math.floor(i / w);
+            return trace(x, y, -1, -1)
+                .concat(trace(x, y, 0, -1))
+                .concat(trace(x, y, 1, -1))
+                .concat(trace(x, y, -1, 0))
+                .concat(trace(x, y, 1, 0))
+                .concat(trace(x, y, -1, 1))
+                .concat(trace(x, y, 0, 1))
+                .concat(trace(x, y, 1, 1));
+        });
+        let seats = '';
+        while (seats != newSeats) {
+            seats = newSeats;
+            newSeats = '';
+            seats.split('').forEach((seat, i) => {
+                if (seat == '.') {
+                    newSeats += '.';
+                    return;
+                }
+                let offsets = seatOffsets[i];
+                let adjacent = offsets.reduce((adjacent, offset) => adjacent + (offset >= 0 && offset < seats.length ? seats[offset] == '#' : 0), 0);
+                if (seat == 'L' && adjacent == 0) {
+                    newSeats += '#';
+                }
+                else if (seat == '#' && adjacent >= 5) {
+                    newSeats += 'L';
+                }
+                else {
+                    newSeats += seat;
+                }
+            });
+        }
+        let occupied = seats.split('').reduce((occupied, seat) => occupied + (seat == '#'), 0);
+        log('Occupied seats (part 2): ' + occupied);
+    }
+};
+
+
 async function run() {
     document.querySelector('pre').textContent = '';
     let day = location.hash.slice(1);
