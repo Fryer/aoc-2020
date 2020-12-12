9424885688f97bc3e2a70bcd40befb4d89eb2d65
Fryer <Fryer@users.noreply.github.com>
2020-12-12 06:41:15 +0100

    Day 12

--- aoc.js
+++ aoc.js
@@ -538,6 +538,89 @@ days[11] = function(data) {
 };
 
 
+days[12] = function(data) {
+    // Part 1:
+    {
+        let moves = data.split('\n').map(move => [move[0], move.slice(1) * 1]);
+        let x = 0;
+        let y = 0;
+        let a = 0;
+        moves.forEach(move => {
+            switch (move[0]) {
+                case 'N':
+                    y += move[1];
+                    break;
+                case 'S':
+                    y -= move[1];
+                    break;
+                case 'E':
+                    x += move[1];
+                    break;
+                case 'W':
+                    x -= move[1];
+                    break;
+                case 'L':
+                    a += move[1] * Math.PI / 180;
+                    break;
+                case 'R':
+                    a -= move[1] * Math.PI / 180;
+                    break;
+                case 'F':
+                    x += move[1] * Math.cos(a);
+                    y += move[1] * Math.sin(a);
+                    break;
+            }
+        });
+        let distance = Math.round(Math.abs(x) + Math.abs(y));
+        log('Distance travelled (part 1): ' + distance);
+    }
+    
+    // Part 2:
+    {
+        let moves = data.split('\n').map(move => [move[0], move.slice(1) * 1]);
+        let x = 0;
+        let y = 0
+        let wx = 10;
+        let wy = 1;
+        moves.forEach(move => {
+            let nwx, nwy;
+            switch (move[0]) {
+                case 'N':
+                    wy += move[1];
+                    break;
+                case 'S':
+                    wy -= move[1];
+                    break;
+                case 'E':
+                    wx += move[1];
+                    break;
+                case 'W':
+                    wx -= move[1];
+                    break;
+                case 'L':
+                    nwx = wx * Math.cos(move[1] * Math.PI / 180) - wy * Math.sin(move[1] * Math.PI / 180);
+                    nwy = wy * Math.cos(move[1] * Math.PI / 180) + wx * Math.sin(move[1] * Math.PI / 180);
+                    wx = nwx;
+                    wy = nwy;
+                    break;
+                case 'R':
+                    nwx = wx * Math.cos(move[1] * Math.PI / 180) + wy * Math.sin(move[1] * Math.PI / 180);
+                    nwy = wy * Math.cos(move[1] * Math.PI / 180) - wx * Math.sin(move[1] * Math.PI / 180);
+                    wx = nwx;
+                    wy = nwy;
+                    break;
+                case 'F':
+                    x += wx * move[1];
+                    y += wy * move[1];
+                    break;
+            }
+        });
+        let distance = Math.round(Math.abs(x) + Math.abs(y));
+        log('Distance travelled (part 2): ' + distance);
+    }
+};
+
+
 async function run() {
     document.querySelector('pre').textContent = '';
     let day = location.hash.slice(1);
