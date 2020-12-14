c67bb7874e8fb0a7b7ff521aff226d06ebeceb60
Fryer <Fryer@users.noreply.github.com>
2020-12-14 11:17:11 +0100

    Day 14

--- aoc.js
+++ aoc.js
@@ -872,6 +872,52 @@ days[13] = function(data) {
 };
 
 
+days[14] = function(data) {
+    // Part 1:
+    {
+        let ops = data.split('\n').map(op => op.split(' = ')).map(op => [op[0].replace(']', '').split('['), op[1]]).map(op => ({ inst: op[0][0], addr: op[0][1], val: op[1] }));
+        let andMask = '1'.repeat(36);
+        let orMask = '0'.repeat(36);
+        let memory = [];
+        ops.forEach(op => {
+            if (op.inst == 'mask') {
+                andMask = op.val.replaceAll('X', '1');
+                orMask = op.val.replaceAll('X', '0');
+            }
+            if (op.inst == 'mem') {
+                memory[op.addr] = (+op.val).toString(2).padStart(36, '0').split('').map((bit, i) => bit & andMask[i] | orMask[i]).join('');
+            }
+        });
+        let sum = memory.reduce((sum, val) => sum + (val ? parseInt(val, 2) : 0), 0);
+        log('Memory sum (part 1): ' + sum);
+    }
+    
+    // Part 2:
+    {
+        let ops = data.split('\n').map(op => op.split(' = ')).map(op => [op[0].replace(']', '').split('['), op[1]]).map(op => ({ inst: op[0][0], addr: op[0][1], val: op[1] }));
+        let mask = '';
+        let memory = {};
+        ops.forEach(op => {
+            if (op.inst == 'mask') {
+                mask = op.val;
+            }
+            if (op.inst == 'mem') {
+                function splitAddr(addr) {
+                    if (addr.indexOf('X') >= 0) {
+                        return [addr.replace('X', '0'), addr.replace('X', '1')].flatMap(addr => splitAddr(addr));
+                    }
+                    return [addr];
+                }
+                let addr = (+op.addr).toString(2).padStart(36, '0').split('').map((bit, i) => mask[i] == '0' ? bit : mask[i]).join('');
+                splitAddr(addr).forEach(addr => memory[addr] = (+op.val).toString(2).padStart(36, '0'));
+            }
+        });
+        let sum = Object.values(memory).reduce((sum, val) => sum + parseInt(val, 2), 0);
+        log('Memory sum (part 2): ' + sum);
+    }
+};
+
+
 async function run() {
     let canvas = document.querySelector('canvas');
     if (canvas) {
