b245c729b5698ee54e4b800ecd0b25c03b1087c4
Fryer <Fryer@users.noreply.github.com>
2020-12-16 07:45:28 +0100

    Day 16

--- aoc.js
+++ aoc.js
@@ -977,6 +977,59 @@ days[15] = {
 };
 
 
+days[16] = {
+    // Part 1:
+    1: function(data) {
+        let [rules, mine, tickets] = data.split('\n\n');
+        rules = rules.split('\n').map(rule => rule.split(': ')[1].split(' or ').map(range => range.split('-')).map(range => ({ low: +range[0], high: +range[1] })));
+        mine = mine.split(':\n')[1].split(',').map(num => +num);
+        tickets = tickets.split(':\n')[1].split('\n').map(ticket => ticket.split(',').map(num => +num));
+        let validNums = new Set(rules.flat().flatMap(range => [...Array(range.high - range.low + 1).keys()].map(num => num + range.low)));
+        let errorRate = tickets.flat().reduce((errorRate, num) => errorRate + (validNums.has(num) ? 0 : num), 0);
+        log('Error rate (part 1): ' + errorRate);
+    },
+    
+    // Part 2:
+    2: function(data) {
+        let [rules, mine, tickets] = data.split('\n\n');
+        let ruleIsDeparture = rules.split('\n').map(rule => rule.split(' ')[0] == 'departure');
+        rules = rules.split('\n').map(rule => rule.split(': ')[1].split(' or ').map(range => range.split('-')).map(range => ({ low: +range[0], high: +range[1] })));
+        mine = mine.split(':\n')[1].split(',').map(num => +num);
+        tickets = tickets.split(':\n')[1].split('\n').map(ticket => ticket.split(',').map(num => +num));
+        let validNums = new Set(rules.flat().flatMap(range => [...Array(range.high - range.low + 1).keys()].map(num => num + range.low)));
+        tickets = tickets.flatMap(ticket => ticket.reduce((valid, num) => valid && validNums.has(num), true) ? [ticket] : []);
+        let fieldRules = tickets[0].map(() => new Set([...Array(rules.length).keys()]));
+        for (let ticket of tickets) {
+            for (let [j, num] of ticket.entries()) {
+                for (let [k, rule] of rules.entries()) {
+                    let match = rule.reduce((match, range) => match || (num >= range.low && num <= range.high), false);
+                    if (!match) {
+                        fieldRules[j].delete(k);
+                    }
+                }
+            }
+        }
+        let fieldRuleIdxs = [];
+        let finished = 0;
+        while (finished < fieldRules.length) {
+            for (let [i, iRuleIdxs] of fieldRules.entries()) {
+                if (iRuleIdxs.size == 1 && fieldRuleIdxs[i] === undefined) {
+                    fieldRuleIdxs[i] = iRuleIdxs.values().next().value;
+                    for (let [j, jRuleIdxs] of fieldRules.entries()) {
+                        if (j != i) {
+                            jRuleIdxs.delete(fieldRuleIdxs[i]);
+                        }
+                    }
+                    finished++;
+                }
+            }
+        }
+        let multiplied = ruleIsDeparture.reduce((multiplied, isDeparture, i) => multiplied * (isDeparture ? mine[fieldRuleIdxs.indexOf(i)] : 1), 1);
+        log('Multiplied departures (part 2): ' + multiplied);
+    }
+};
+
+
 async function run() {
     let hash = location.hash.slice(1).split('-');
     let selected = hash[0] > 0 && hash[0] <= 25 ? hash[0] : days.length - 1;
