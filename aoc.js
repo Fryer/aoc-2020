7b7e47aae829f6151f4ed7beb2f7d5118b325353
Fryer <Fryer@users.noreply.github.com>
2020-12-22 07:13:50 +0100

    Day 22

--- aoc.js
+++ aoc.js
@@ -1533,6 +1533,57 @@ days[21] = {
 };
 
 
+days[22] = {
+    // Part 1:
+    1: function(data) {
+        let players = data.split('\n\n').map(player => player.split(':\n')[1]).map(player => player.split('\n').map(card => +card));
+        while (players[0].length > 0 && players[1].length > 0) {
+            let topA = players[0].shift();
+            let topB = players[1].shift();
+            if (topA > topB) {
+                players[0].push(topA, topB);
+            }
+            else {
+                players[1].push(topB, topA);
+            }
+        }
+        let winner = players[0].length ? players[0] : players[1];
+        let score = winner.reverse().reduce((score, card, i) => score += card * (i + 1), 0);
+        log('Winning score (part 1): ' + score);
+    },
+    
+    // Part 2:
+    2: function(data) {
+        let players = data.split('\n\n').map(player => player.split(':\n')[1]).map(player => player.split('\n').map(card => +card));
+        function play(players) {
+            let previous = new Set();
+            while (players[0].length > 0 && players[1].length > 0) {
+                if (previous.has(players[0] + ' ' + players[1])) {
+                    return 0;
+                }
+                previous.add(players[0] + ' ' + players[1]);
+                let topA = players[0].shift();
+                let topB = players[1].shift();
+                let winner = topA > topB ? 0 : 1;
+                if (players[0].length >= topA && players[1].length >= topB) {
+                    winner = play([players[0].slice(0, topA), players[1].slice(0, topB)]);
+                }
+                if (winner == 0) {
+                    players[0].push(topA, topB);
+                }
+                else {
+                    players[1].push(topB, topA);
+                }
+            }
+            return players[0].length ? 0 : 1;
+        }
+        let winner = play(players, 1);
+        let score = players[winner].reverse().reduce((score, card, i) => score += card * (i + 1), 0);
+        log('Winning score (part 2): ' + score);
+    }
+};
+
+
 async function run() {
     let hash = location.hash.slice(1).split('-');
     let selected = hash[0] == 'editor' || (hash[0] > 0 && hash[0] < days.length) ? hash[0] : days.length - 1;
