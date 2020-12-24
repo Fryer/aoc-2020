da97fc39dd1a62990db1ffbbb761d257fb6cb356
Fryer <Fryer@users.noreply.github.com>
2020-12-24 06:38:47 +0100

    Day 24

--- aoc.js
+++ aoc.js
@@ -1653,6 +1653,124 @@ days[23] = {
 };
 
 
+days[24] = {
+    // Part 1:
+    1: function(data) {
+        let instructions = data.split('\n');
+        let flipped = new Set();
+        for (let instruction of instructions) {
+            let x = 0;
+            let y = 0;
+            for (let i = 0; i < instruction.length; i++) {
+                let c = instruction[i];
+                let n = instruction[i + 1];
+                if (c == 'n') {
+                    y--;
+                    if (n == 'w') {
+                        x--;
+                    }
+                    i++;
+                }
+                else if (c == 's') {
+                    y++;
+                    if (n == 'e') {
+                        x++;
+                    }
+                    i++;
+                }
+                else if (c == 'w') {
+                    x--;
+                }
+                else if (c == 'e') {
+                    x++;
+                }
+            }
+            if (flipped.has([x, y].toString())) {
+                flipped.delete([x, y].toString());
+            }
+            else {
+                flipped.add([x, y].toString());
+            }
+        }
+        log('Black tiles (part 1): ' + flipped.size);
+    },
+    
+    // Part 2:
+    2: function(data) {
+        let instructions = data.split('\n');
+        let flipped = new Set();
+        let tiles = {};
+        for (let instruction of instructions) {
+            let x = 0;
+            let y = 0;
+            for (let i = 0; i < instruction.length; i++) {
+                let c = instruction[i];
+                let n = instruction[i + 1];
+                if (c == 'n') {
+                    y--;
+                    if (n == 'w') {
+                        x--;
+                    }
+                    i++;
+                }
+                else if (c == 's') {
+                    y++;
+                    if (n == 'e') {
+                        x++;
+                    }
+                    i++;
+                }
+                else if (c == 'w') {
+                    x--;
+                }
+                else if (c == 'e') {
+                    x++;
+                }
+            }
+            if (flipped.has([x, y].toString())) {
+                flipped.delete([x, y].toString());
+            }
+            else {
+                flipped.add([x, y].toString());
+                tiles[[x, y]] = [x, y];
+                tiles[[x - 1, y - 1]] = [x - 1, y - 1];
+                tiles[[x, y - 1]] = [x, y - 1];
+                tiles[[x, y + 1]] = [x, y + 1];
+                tiles[[x + 1, y + 1]] = [x + 1, y + 1];
+                tiles[[x - 1, y]] = [x - 1, y];
+                tiles[[x + 1, y]] = [x + 1, y];
+            }
+        }
+        for (let i = 0; i < 100; i++) {
+            let newFlipped = new Set();
+            let newTiles = {};
+            for (let [x, y] of Object.values(tiles)) {
+                let neighbors = 0;
+                neighbors += flipped.has([x - 1, y - 1].toString());
+                neighbors += flipped.has([x, y - 1].toString());
+                neighbors += flipped.has([x, y + 1].toString());
+                neighbors += flipped.has([x + 1, y + 1].toString());
+                neighbors += flipped.has([x - 1, y].toString());
+                neighbors += flipped.has([x + 1, y].toString());
+                if ((flipped.has([x, y].toString()) && neighbors != 0 && neighbors <= 2) || (!flipped.has([x, y].toString()) && neighbors == 2)) {
+                    newFlipped.add([x, y].toString());
+                    newTiles[[x, y]] = [x, y];
+                    newTiles[[x - 1, y - 1]] = [x - 1, y - 1];
+                    newTiles[[x, y - 1]] = [x, y - 1];
+                    newTiles[[x, y + 1]] = [x, y + 1];
+                    newTiles[[x + 1, y + 1]] = [x + 1, y + 1];
+                    newTiles[[x - 1, y]] = [x - 1, y];
+                    newTiles[[x + 1, y]] = [x + 1, y];
+                }
+            }
+            tiles = newTiles;
+            flipped = newFlipped;
+        }
+        log('Black tiles (part 1): ' + flipped.size);
+    }
+};
+
+
 async function run() {
     let hash = location.hash.slice(1).split('-');
     let selected = hash[0] == 'editor' || (hash[0] > 0 && hash[0] < days.length) ? hash[0] : days.length - 1;
